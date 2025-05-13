9a13976bc85ac9419960afd6deee4435fe6218ed
biscakine <102854948+biscakine@users.noreply.github.com>
2025-05-13 13:01:42 +0200

    Update test-input-page.ts

--- app/views/test/test-input-page.ts
+++ app/views/test/test-input-page.ts
@@ -2,7 +2,12 @@ import { EventData, Page } from '@nativescript/core';
 import { TestInputViewModel } from './test-input-view-model';
 
 export function navigatingTo(args: EventData) {
-    const page = <Page>args.object;
-    const context = page.navigationContext;
-    page.bindingContext = new TestInputViewModel(context);
+  const page = args.object as Page;
+  const ctx  = page.navigationContext;        // { participantId, sessionId, … }
+  page.bindingContext = new TestInputViewModel(ctx);
+}
+
+export function navigatingFrom(args: EventData) {
+  const page = args.object as Page;
+  (page.bindingContext as TestInputViewModel).dispose();
 }
