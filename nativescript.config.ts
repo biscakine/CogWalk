9fa4826bb304df240e3faa79c7865fcd707259ff
biscakine <102854948+biscakine@users.noreply.github.com>
2025-05-13 11:46:49 +0200

    Update nativescript.config.ts

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -5,13 +5,14 @@ export default {
   appPath: 'app',
   appResourcesPath: '../../tools/assets/App_Resources',
   android: {
+    compileSdk: 33,   // ← NOUVEAU
+    minSdk: 21,       // (ou 22 si vous préférez)
+    targetSdk: 33,    // ← cohérent
     v8Flags: '--expose_gc',
     markingMode: 'none'
   },
   webpackConfigPath: 'webpack.config.js',
   useLibs: true,
   previewAppSchema: 'ns-preview',
-  preview: {
-    port: 3000
-  }
+  preview: { port: 3000 }
 } as NativeScriptConfig;
