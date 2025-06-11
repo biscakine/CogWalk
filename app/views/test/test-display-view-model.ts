de2e23a00ddc6ba00bf2b6c6e703e3d70883f2ec
BLH64 <basil.lafitte.houssat@gmail.com>
2025-06-11 16:16:51 +0200

    Update test-display-view-model.ts

--- app/views/test/test-display-view-model.ts
+++ app/views/test/test-display-view-model.ts
@@ -1,46 +1,32 @@
 import { Observable, Frame } from '@nativescript/core';
-import { PhrasesService } from '../../services/phrases.service';
+import { PhrasesService }       from '../../services/phrases.service';
 
 export interface TestDisplayContext {
-  originalText?: string;
   participantId?: string;
-  sessionId?:   string;
-  wordCount?:   number;
+  sessionId?:     string;
 }
 
 export class TestDisplayViewModel extends Observable {
-  private phrasesSvc = new PhrasesService();
-  public phrase = '';
-
+  public phrase = "";
+  private svc   = new PhrasesService();
   private ctx: TestDisplayContext;
 
   constructor(context: TestDisplayContext) {
     super();
     this.ctx = context || {};
-
-    if (this.ctx.originalText) {
-      // Si on reçoit déjà une phrase en contexte, on l'affiche
-      this.phrase = this.ctx.originalText;
-    } else {
-      // Sinon on prend la première d'une liste mélangée
-      const all = this.phrasesSvc.getAllPhrases();
-      const shuffled = all.sort(() => Math.random() - 0.5);
-      this.phrase = shuffled[0];
-    }
+    // À CHAQUE navigation vers cette page, on prend la phrase suivante
+    this.phrase = this.svc.getRandomPhrase();
     this.notifyPropertyChange('phrase', this.phrase);
   }
 
-  /** Quand l’utilisateur a mémorisé, on passe à la saisie */
   public onPhraseMemorized(): void {
     Frame.topmost().navigate({
       moduleName: 'views/test/test-input-page',
       context: {
         participantId: this.ctx.participantId,
         sessionId:     this.ctx.sessionId,
-        // on ré-expédie la même phrase pour le premier tour si besoin :
-        originalText:  this.phrase
+        originalText:  this.phrase  // on l’envoie pour inference si besoin
       },
-      clearHistory: false,
       transition: { name: 'slide', duration: 200 }
     });
   }
