032b73c4bee1f203aa887bb604e2e1ce5f1b2766
BLH64 <basil.lafitte.houssat@gmail.com>
2025-06-11 15:06:19 +0200

    Update test-display-view-model.ts

--- app/views/test/test-display-view-model.ts
+++ app/views/test/test-display-view-model.ts
@@ -1,39 +1,47 @@
 import { Observable, Frame } from '@nativescript/core';
+import { PhrasesService } from '../../services/phrases.service';
 
 export interface TestDisplayContext {
   originalText?: string;
   participantId?: string;
-  sessionId?: string;
-  wordCount?: number;
+  sessionId?:   string;
+  wordCount?:   number;
 }
 
 export class TestDisplayViewModel extends Observable {
-  public phrase: string = '';
+  private phrasesSvc = new PhrasesService();
+  public phrase = '';
 
   private ctx: TestDisplayContext;
 
   constructor(context: TestDisplayContext) {
     super();
     this.ctx = context || {};
-    // Récupère la phrase à mémoriser depuis le contexte
-    this.phrase = this.ctx.originalText || '';
+
+    if (this.ctx.originalText) {
+      // Si on reçoit déjà une phrase en contexte, on l'affiche
+      this.phrase = this.ctx.originalText;
+    } else {
+      // Sinon on prend la première d'une liste mélangée
+      const all = this.phrasesSvc.getAllPhrases();
+      const shuffled = all.sort(() => Math.random() - 0.5);
+      this.phrase = shuffled[0];
+    }
     this.notifyPropertyChange('phrase', this.phrase);
   }
 
-  /** Appelé quand l’utilisateur clique sur “J’ai mémorisé” */
+  /** Quand l’utilisateur a mémorisé, on passe à la saisie */
   public onPhraseMemorized(): void {
     Frame.topmost().navigate({
       moduleName: 'views/test/test-input-page',
       context: {
         participantId: this.ctx.participantId,
         sessionId:     this.ctx.sessionId,
-        wordCount:     this.ctx.wordCount
+        // on ré-expédie la même phrase pour le premier tour si besoin :
+        originalText:  this.phrase
       },
       clearHistory: false,
-      transition: {
-        name: 'slide',
-        duration: 200
-      }
+      transition: { name: 'slide', duration: 200 }
     });
   }
 }
