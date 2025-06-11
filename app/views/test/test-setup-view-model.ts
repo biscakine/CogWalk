615742d95c463a6a588a1673b1501f927a9d806f
BLH64 <basil.lafitte.houssat@gmail.com>
2025-06-11 11:17:07 +0200

    Update test-setup-view-model.ts
    problème bouton commencer le test

--- app/views/test/test-setup-view-model.ts
+++ app/views/test/test-setup-view-model.ts
@@ -1,36 +1,37 @@
 import { Observable, Frame } from '@nativescript/core';
 
 export class TestSetupViewModel extends Observable {
-    private _wordCount: number = 3;
-    private participant: any;
+  private _wordCount = 3;
+  private participant: any;
 
-    constructor(context: { participant: any }) {
-        super();
-        this.participant = context.participant;
-    }
+  constructor(context: { participant: any }) {
+    super();
+    this.participant = context.participant;
+  }
 
-    get wordCount(): number {
-        return this._wordCount;
-    }
+  get wordCount(): number {
+    return this._wordCount;
+  }
 
-    selectWordCount(args: any) {
-        const button = args.object;
-        this._wordCount = parseInt(button.text, 10);
-        this.notifyPropertyChange('wordCount', this._wordCount);
-    }
+  selectWordCount(args: any): void {
+    const button = args.object as any;
+    this._wordCount = parseInt(button.text, 10);
+    this.notifyPropertyChange('wordCount', this._wordCount);
+  }
 
-    onStartTest() {
-        Frame.topmost().navigate({
-            moduleName: 'views/test/test-display-page',
-            context: {
-                wordCount: this._wordCount,
-                participant: this.participant
-            },
-            clearHistory: false,
-            transition: {
-                name: 'slide',
-                duration: 200
-            }
-        });
-    }
+  onStartTest(): void {
+    Frame.topmost().navigate({
+      // on cible directement la page de saisie
+      moduleName: 'views/test/test-input-page',
+      context: {
+        participantId: this.participant.id,
+        wordCount:     this._wordCount
+      },
+      clearHistory: false,
+      transition: {
+        name: 'slide',
+        duration: 200
+      }
+    });
+  }
 }
