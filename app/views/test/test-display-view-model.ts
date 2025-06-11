0ab877873bc10daf6eba4dd459ccca738979e683
BLH64 <basil.lafitte.houssat@gmail.com>
2025-06-11 16:40:39 +0200

    Update test-display-view-model.ts

--- app/views/test/test-display-view-model.ts
+++ app/views/test/test-display-view-model.ts
@@ -1,5 +1,5 @@
 import { Observable, Frame } from '@nativescript/core';
-import { PhrasesService }       from '../../services/phrases.service';
+import { PhrasesService } from '../../services/phrases.service';
 
 export interface TestDisplayContext {
   participantId?: string;
@@ -7,26 +7,28 @@ export interface TestDisplayContext {
 }
 
 export class TestDisplayViewModel extends Observable {
-  public phrase = "";
-  private svc   = new PhrasesService();
+  private svc    = new PhrasesService();
+  public  phrase = '';
   private ctx: TestDisplayContext;
 
   constructor(context: TestDisplayContext) {
     super();
-    this.ctx = context || {};
-    // À CHAQUE navigation vers cette page, on prend la phrase suivante
+    this.ctx    = context || {};
+    // À chaque entrée sur cette page, on prend la phrase suivante
     this.phrase = this.svc.getRandomPhrase();
     this.notifyPropertyChange('phrase', this.phrase);
   }
 
+  /** Quand l’utilisateur a mémorisé, on navigue vers la saisie */
   public onPhraseMemorized(): void {
     Frame.topmost().navigate({
       moduleName: 'views/test/test-input-page',
       context: {
         participantId: this.ctx.participantId,
         sessionId:     this.ctx.sessionId,
-        originalText:  this.phrase  // on l’envoie pour inference si besoin
+        originalText:  this.phrase
       },
+      clearHistory: false,
       transition: { name: 'slide', duration: 200 }
     });
   }
