bfe603ba8e3093c9f858674015955ca908dd3aae
BLH64 <basil.lafitte.houssat@gmail.com>
2025-06-12 12:02:48 +0200

    Update participant-results-page.ts

--- app/views/participant/participant-results-page.ts
+++ app/views/participant/participant-results-page.ts
@@ -2,7 +2,7 @@ import { EventData, Page } from '@nativescript/core';
 import { ParticipantResultsViewModel } from './participant-results-view-model';
 
 export function navigatingTo(args: EventData) {
-    const page = <Page>args.object;
-    const context = page.navigationContext;
-    page.bindingContext = new ParticipantResultsViewModel(context.participant);
+  const page = args.object as Page;
+  const context = page.navigationContext; // devrait contenir `participant` objet
+  page.bindingContext = new ParticipantResultsViewModel(context.participant);
 }
